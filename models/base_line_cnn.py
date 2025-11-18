ecb51088d7a0c4cfb5dfaf4e1962783b3bd67ed5
dmytro <dmytro19022006@gmail.com>
2025-11-18 21:52:16 +0200

    base line CNN

---
+++ models/base_line_cnn.py
@@ -0,0 +1,51 @@
+import torch
+import torch.nn as nn
+
+class BaseLineCNN(nn.Module):
+    """Base line CNN for diabetic rethinopathy recognition"""
+    def __init__(self, n_classes: int = 5):
+        super().__init__()
+        # conv layers
+        self.featuresExtraction = nn.Sequential(
+            nn.Conv2d(3, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            
+            nn.Conv2d(32, 64, kernel_size=3, stride=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+
+            nn.Conv2d(64, 64, kernel_size=3),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            
+            nn.Conv2d(64, 128, kernel_size=3, stride=2),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+
+            nn.Conv2d(128, 128, kernel_size=3),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+        )
+        # avg pool
+        self.avgpool = nn.AdaptiveAvgPool2d((4, 4))
+        # classification
+        self.classifier = nn.Sequential(
+            nn.Linear(128 * 4 * 4, 256),
+            nn.ReLU(),
+            nn.Linear(256, n_classes),
+        )
+
+    def forward(self, X: torch.Tensor):
+        """
+        Model forward pass.
+        - Feature extraction:
+            Conv2d -> BatchNorm -> ReLU (n times)
+        - Classification:
+            Linear -> ReLU -> Linear
+        """
+        X = self.featuresExtraction(X)
+        X = self.avgpool(X)
+        X = torch.flatten(X, 1)
+        X = self.classifier(X)
+        return X
